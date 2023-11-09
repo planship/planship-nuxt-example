b2130acb9aebf24fe9d156ec39c446353cefec06
Pawel Wojnarowicz <pawel.wojnarowicz@gmail.com>
2023-11-09 07:03:44 -0800

    Moved app contents to the root dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
     public: {
       planshipApiClientId: process.env.PLANSHIP_API_CLIENT_ID || '973NYSQ4GQJZ8JBFKIOK',
       clientPlanshipApiUrl: process.env.PLANSHIP_API_CLIENT_URL || 'http://localhost:8002',
-      serverPlanshipApiUrl: process.env.PLANSHIP_API_SERVER_URL || 'http://main:8002',
+      serverPlanshipApiUrl: process.env.PLANSHIP_API_SERVER_URL || 'http://localhost:8002',
     },
   }
 })

8efeef2b7ebd149cc8e9d015e1f817641bbfa77e
Pawel Wojnarowicz <pawel.wojnarowicz@gmail.com>
2023-12-04 21:42:26 +0100

    Remove API key/secret from nuxt config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,10 +30,10 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
-    planshipApiClientSecret: process.env.PLANSHIP_API_CLIENT_SECRET || 'RJSazPD8NEM5PEzIl8JoXIRJNZm3uAhX',
+    planshipApiClientSecret: process.env.PLANSHIP_API_CLIENT_SECRET,
 
     public: {
-      planshipApiClientId: process.env.PLANSHIP_API_CLIENT_ID || '973NYSQ4GQJZ8JBFKIOK',
+      planshipApiClientId: process.env.PLANSHIP_API_CLIENT_ID,
       clientPlanshipApiUrl: process.env.PLANSHIP_API_CLIENT_URL || 'http://localhost:8002',
       serverPlanshipApiUrl: process.env.PLANSHIP_API_SERVER_URL || 'http://localhost:8002',
       webSocketUrl: process.env.WEBSOCKET_URL || undefined,

23f14725d5e35248f2388f2bae6659d490a1a4da
Pawel Wojnarowicz <pawel.wojnarowicz@gmail.com>
2023-12-05 21:08:40 +0100

    Change product name to 'clicker-demo'

--- server/api/planshipToken.ts
+++ server/api/planshipToken.ts
@@ -1,7 +1,7 @@
 import { Planship, TokenResponse } from '@planship/fetch'
 
 export default defineEventHandler((event) => {
-  const client = new Planship('clicker', useRuntimeConfig().public.serverPlanshipApiUrl, useRuntimeConfig().public.planshipApiClientId , useRuntimeConfig().planshipApiClientSecret)
+  const client = new Planship('clicker-demo', useRuntimeConfig().public.serverPlanshipApiUrl, useRuntimeConfig().public.planshipApiClientId , useRuntimeConfig().planshipApiClientSecret)
   return client.getAccessToken().then((tokenData: TokenResponse) => {
     return tokenData.accessToken
   })

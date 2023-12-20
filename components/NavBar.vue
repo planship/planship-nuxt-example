d67558e5bfc7439f9c1cfdf164a635b431b6b4c6
Pawel Wojnarowicz <pawel.wojnarowicz@gmail.com>
2023-12-20 21:49:54 +0100

    Display customer ID in the user menu instead of name.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -68,7 +68,7 @@ const { currentPlanName, entitlements } = storeToRefs(planshipStore)
             >
               <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100">
                 <MenuItem>
-                  <span class="block px-4 py-2 text-sm text-gray-700">Signed in as <b>{{ currentUser.name }}</b></span>
+                  <span class="block px-4 py-2 text-sm text-gray-700">Signed in as <b>{{ currentUser.id }}</b></span>
                 </MenuItem>
                 <MenuItem v-slot="{ active, close }" as="div">
                   <NuxtLink
